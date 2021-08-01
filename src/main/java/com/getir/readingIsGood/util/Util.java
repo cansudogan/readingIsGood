b6b9198b0c7a2219d850e59d1e0a21493daf13b5
cansudogan <cansudogan95@gmail.com>
2021-08-01 18:11:26 +0300

    get customer id by token

---
+++ src/main/java/com/getir/readingIsGood/util/Util.java
@@ -0,0 +1,12 @@
+package com.getir.readingIsGood.util;
+
+import com.getir.readingIsGood.security.UserDetailsImpl;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class Util {
+    public static Long getUserId(){
+        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication()
+                .getPrincipal();
+        return userDetails.getId();
+    }
+}
